745fc88635f5c22129f1532dc24a708929d5335e
unknown <redoyk2015@gmail.com>
2022-08-18 02:37:59 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//field section
+document.getElementById('input-field').addEventListener('keyup', function (event) {
+
+    const keys = event.target.value;
+
+    const headText = document.getElementById('head1');
+    headText.innerText = keys;
+
+});
+
+//button section
+
+document.getElementById('btn').addEventListener('click', function () {
+
+    let f1 = document.getElementById('input-field');
+    let value = f1.value;
+
+    let f2 = document.getElementById('input-field2');
+    let value2 = f2.value;
+
+    let f3 = document.getElementById('input-field3');
+    let value3 = f3.value;
+
+    if (value2 === 'DOM30') {
+
+        let percentence = (value * value3) / 100;
+        let parcentParse = parseFloat(percentence);
+
+        document.getElementById('head2').innerText = ' $' + parcentParse;
+    }
+
+    else {
+
+        document.getElementById('head2').innerText = ' Wrong Code';
+    }
+
+});
